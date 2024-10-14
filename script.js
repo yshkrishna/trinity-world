6d6d2d41fe580e288080a54546d8593963dbf43d
HariKrishna <yshkrishna@gmail.com>
2024-10-14 14:46:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,4 @@ $(document).ready(function(){
            scrollTop: $($.attr(this, 'href')).offset().top
        }, 500);
    });
-
-   // Animate cards on scroll
-   $(window).on('scroll', function() {
-       $('.card').each(function() {
-           var cardPos = $(this).offset().top,
-               winScrollTop = $(window).scrollTop(),
-               winHeight = $(window).height();
-
-           if (cardPos <= winScrollTop + winHeight - 100) { // Trigger when card is in view
-               $(this).addClass('visible');
-           }
-       });
-   });
 });
